204dd57bc501d6b1e1c42f5fe87e5387084ef47b
sauravionic <srivastava2000saurav@gmail.com>
2023-07-28 12:43:41 +0530

    Added routes to server.js

--- api/server.js
+++ api/server.js
@@ -2,6 +2,15 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import authRoute from "./routes/auth.route";
+import userRoute from "./routes/user.route";
+import gigRoute from "./routes/gig.route";
+import orderRoute from "./routes/order.route";
+import conversationRoute from "./routes/conversation.route";
+import messageRoute from "./routes/message.route";
+import reviewRoute from "./routes/review.route";
+
+
 
 const app = express();
 dotenv.config();
@@ -16,6 +25,14 @@ const connectToDB = async () => {
         console.log(error);
     }
 }
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+app.use("/api/gigs", gigRoute);
+app.use("/api/orders", orderRoute);
+app.use("/api/conversations", conversationRoute);
+app.use("/api/messages", messageRoute);
+app.use("/api/reviews", reviewRoute);
 
 // Connections
 app.listen(process.env.PORT, () => {
