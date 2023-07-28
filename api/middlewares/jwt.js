007dec5d20371adc2e5534a81b9f13ec57803db1
sauravionic <srivastava2000saurav@gmail.com>
2023-07-28 17:29:02 +0530

    Add jwt middleware to the project. Closes #9

---
+++ api/middlewares/jwt.js
@@ -0,0 +1,15 @@
+import jwt from "jsonwebtoken";
+import createError from "../utils/createError.js";
+
+export const verifyToken = (req, res, next) => { 
+    const token = req.cookies.accessToken;
+    if (!token) return next(createError(401, "You are not authenticated"));
+    jwt.verify(token, process.env.JWT_KEY, async (err, payload) => {
+        if (err) return next(createError(403, "Token is not valid"));
+        req.userId = payload.id;
+        req.isSeller = payload.isSeller;
+
+        //Using next here so that it can go to the next function which has been defined like deleteUser etc a verifyToken is just a middleware
+        next();
+    })
+}
